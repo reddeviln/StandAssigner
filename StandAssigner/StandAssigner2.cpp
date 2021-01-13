7477fc4df76b06d94e3d8cba221c24dffafa86e0
reddeviln <nilsdornbusch@yahoo.de>
2021-01-13 19:34:46 +0100

    manual assignment

--- StandAssigner/StandAssigner2.cpp
+++ StandAssigner/StandAssigner2.cpp
@@ -11,6 +11,7 @@
 #include <filesystem>
 #include "loguru.cpp"
 #include <unordered_map>
+#include <utility>
 
 
 
@@ -23,12 +24,15 @@
 #define MY_PLUGIN_VIEW      ""
 
 
-const	int		TAG_ITEM_ROUTE_VALID = 123123;
-
+const	int		TAG_ITEM_STAND = 15278;
+std::unordered_map<std::string, Stand> data;
+std::unordered_map<std::string, Stand> standmapping;
 
 const   int     TAG_FUNC_EDIT = 15;
 const int TAG_FUNC_ASSIGN_POPUP = 16;
-
+const int TAG_FUNC_MANUAL_FINISH = 22;
+const int TAG_FUNC_CLEAR = 264;
+const double TOL = 0.02;
 
 
 //---CGMPHelper------------------------------------------
@@ -68,26 +72,29 @@ CStandAssigner::CStandAssigner(void)
 	loguru::add_file(logpath, loguru::FileMode::Truncate, loguru::Verbosity_INFO);
 	LOG_F(INFO, "We successfully started StandAssigner. Opa!");
 
-	RegisterTagItemType("Stand", TAG_ITEM_ROUTE_VALID);
+	RegisterTagItemType("Stand", TAG_ITEM_STAND);
 
 	RegisterTagItemFunction("Edit Stand", TAG_FUNC_EDIT);
 
 	RegisterTagItemFunction("Assign Stand", TAG_FUNC_ASSIGN_POPUP);
 
-
+	RegisterTagItemFunction("Clear", TAG_FUNC_CLEAR);
 
 
 	LOG_F(INFO, "Everything registered. Ready to go!");
 
 
 	dir += "OMDB.csv";
-	io::CSVReader<5, io::trim_chars<' '>, io::no_quote_escape<','>> in(dir);
-	in.read_header(io::ignore_extra_column, "Dep", "Dest", "Evenodd", "Restriction", "Route");
-	std::string Dep, Dest, evenodd, LevelR, Routing;
-	while (in.read_row(Dep, Dest, evenodd, LevelR, Routing))
+	io::CSVReader<8, io::trim_chars<' '>, io::no_quote_escape<','>> in(dir);
+	in.read_header(io::ignore_extra_column, "Standnumber", "latitude", "longitude", "airlinecode", "neighbor1", "neighbor2","size","flytampa");
+	std::string StandNumber, lat, Long, airlinecode, neighbor1, neighbor2, size, flytampa;
+	while (in.read_row(StandNumber, lat, Long, airlinecode, neighbor1, neighbor2, size, flytampa))
 	{
-		
+		Stand temp =  Stand(StandNumber, lat, Long, airlinecode, neighbor1, neighbor2, size, flytampa);
+		std::pair<std::string,Stand> temp2(StandNumber, temp);
+		data.insert(temp2);
 	}
+	LOG_F(INFO, "Stand file read without issues!");
 }
 
 
@@ -95,6 +102,16 @@ CStandAssigner::CStandAssigner(void)
 
 CStandAssigner::~CStandAssigner(void)
 {
+}
+std::string getStandOfAircraft(EuroScopePlugIn::CPosition position)
+{
+	for (auto stand : data)
+	{
+		auto distance = position.DistanceTo(stand.second.position);
+		if (distance < TOL)
+			return stand.first;
+	}
+	return "-1";
 }
 
 void CStandAssigner::OnGetTagItem(EuroScopePlugIn::CFlightPlan FlightPlan,
@@ -106,11 +123,57 @@ void CStandAssigner::OnGetTagItem(EuroScopePlugIn::CFlightPlan FlightPlan,
 	COLORREF * pRGB,
 	double * pFontSize)
 {
-	
+	if (!FlightPlan.IsValid())
 		return;
-	
+	switch (ItemCode)
+	{
+	case TAG_ITEM_STAND:
+		
+		auto found = standmapping.find(FlightPlan.GetCallsign());
+		if (found != standmapping.end())
+		{
+			auto temp = found->second.number;
+			strcpy(sItemString, temp.c_str());
+		}
+				
+	}
+		
+}
+bool CStandAssigner::OnCompileCommand(const char * sCommandLine)
+{
+	if (std::strcmp(sCommandLine, ".showstands") == 0)
+	{
+		std::string out;
+		for (auto it : data)
+		{
+			if (!it.second.isEmpty || it.second.isAssigned)
+				out += it.first;
+		}
+		DisplayUserMessage("Occupied Stands", "OMDB", out.c_str() , true, true, true, true, true);
+		return true;
+	}
+
+}
+void CStandAssigner::OnRadarTargetPositionUpdate(EuroScopePlugIn::CRadarTarget rt)
+{
+	if (rt.IsValid())
+	{
+		auto position = rt.GetPosition().GetPosition();
+		auto stand = getStandOfAircraft(position);
+		if (stand != "-1") {
+			data.at(stand).isEmpty = false;
+		}
+	}
 }
 
+
+void  CStandAssigner::OnTimer(int Counter)
+{
+	if (Counter % 10 == 0)
+	{
+		cleanupStands();
+	}
+}
 
 
 inline void CStandAssigner::OnFunctionCall(int FunctionId, const char * sItemString, POINT Pt, RECT Area)
@@ -127,17 +190,72 @@ inline void CStandAssigner::OnFunctionCall(int FunctionId, const char * sItemStr
 
 	// select it from the sequence
 
-	auto fpdata = fp.GetFlightPlanData();
-	std::string dep = fpdata.GetOrigin();
-
 	// switch by the function ID
 	switch (FunctionId)
 	{
-
+	case TAG_FUNC_EDIT:
+		OpenPopupEdit(Area,
+			TAG_FUNC_MANUAL_FINISH,
+			"");
+		break;
+	case TAG_FUNC_MANUAL_FINISH:
+	{
+		std::string input = sItemString;
+		try {
+			data.at(input).isAssigned = true;
+			std::pair<std::string, Stand> temp2(fp.GetCallsign(), data.at(input));
+			standmapping.insert(temp2);
+			break;
+		}
+		catch (std::exception e)
+		{
+			break;
+		}
+	}
+	case TAG_FUNC_CLEAR:
+	{
+		auto found = standmapping.find(fp.GetCallsign());
+		if (found != standmapping.end())
+		{
+			auto temp2 = data.find(found->second.number);
+			if (temp2 != data.end())
+			{
+				temp2->second.isAssigned = false;
+			}
+			standmapping.erase(found);
+			
+		}
+	}
 	}// switch by the function ID
 }
+void CStandAssigner::cleanupStands()
+{
+	for (auto& it : data)
+	{
+		
+		if (!it.second.isEmpty)
+		{
+			auto first = RadarTargetSelectFirst();
+			if (first.GetPosition().GetPosition().DistanceTo(it.second.position) < TOL)
+				goto outer;
+			auto temp = RadarTargetSelectNext(first);
+			while (temp.IsValid())
+			{
+				if (temp.GetPosition().GetPosition().DistanceTo(it.second.position) < TOL)
+				{
+					goto outer;
+				}
+					
+				temp = RadarTargetSelectNext(temp);
+			}
+			it.second.isEmpty = true;
+		}
+	outer:
+		continue;
+	}
+ }
 
-void 
+
 
 
 
--- StandAssigner/StandAssigner2.h
+++ StandAssigner/StandAssigner2.h
@@ -5,17 +5,85 @@
 #include <vector>
 #include "csv.h"
 #include "loguru.hpp"
+#define INT_PART 4
+#define DEC_PART 3
 
 
+class Stand
+{
+	//this class stores all information regarding one particular stand
+public:
+	std::string number;
+	double mlat, mLong;
+	EuroScopePlugIn::CPosition position;
+	std::string mAirlinecode;
+	std::string mNeighbor1;
+	std::string mNeighbor2;
+	bool isEmpty = true; 
+	bool isAssigned = false;
+	char mSize;
+	bool isInFlytampa = false;
+	Stand(std::string StandNumber, std::string lat, std::string Long, std::string airlinecode, std::string neighbor1, std::string neighbor2, std::string size, std::string flytampa)
+	{
+		number = StandNumber;
+		mlat = Str2LatLong(lat);
+		mLong = Str2LatLong(Long);
+		position = EuroScopePlugIn::CPosition();
+		position.m_Latitude = mlat;
+		position.m_Longitude = mLong;
+		mNeighbor1 = neighbor1;
+		mNeighbor2 = neighbor2;
+		
+		mSize = size.at(0);
+		mAirlinecode = airlinecode;
+		if (flytampa == "yes")
+		{
+			isInFlytampa = true;
+		}
+		else if (flytampa == "no")
+		{
+			isInFlytampa = false;
+		}
+	}
 
-class CStandAssigner :
+	double Str2LatLong(std::string coord)
+
+	{
+		std::vector<double> splitted;
+		std::string delimiter = "-";
+		size_t pos = 0;
+		std::string token;
+		while ((pos = coord.find(delimiter)) != std::string::npos) {
+			token = coord.substr(0, pos);
+			switch (token.at(0))
+			{
+			case 'N':
+			case 'E':
+				token = token.substr(1, token.length() - 1);
+				splitted.push_back(std::stod(token));
+				break;
+			case 'S':
+			case 'W':
+				token = token.substr(1, token.length() - 1);
+				splitted.push_back(std::stod(token)*-1);
+			default:
+				splitted.push_back(std::stod(token));
+			}
+			coord.erase(0, pos + delimiter.length());
+		}
+		splitted.push_back(std::stod(coord));
+		return splitted[0] + splitted[1] / 60 + splitted[2] / 3600;
+	}
+
+};
+class CStandAssigner:
 	//The class that holds all our functions 
 	public EuroScopePlugIn::CPlugIn
 {
 public:
 	//the list displayed in euroscope
 
-
+	
 
 	CStandAssigner(void);
 
@@ -32,6 +100,7 @@ public:
 	{
 		return true;
 	}
+
 	bool fileExists(const std::string& filename)
 	{
 		struct stat buf;
@@ -63,8 +132,13 @@ public:
 		const char * sItemString,
 		POINT Pt,
 		RECT Area);
+	inline  virtual void    OnRadarTargetPositionUpdate(EuroScopePlugIn::CRadarTarget RadarTarget);
 
 
-
+	inline  virtual bool    OnCompileCommand(const char * sCommandLine);
+	inline  virtual void    OnTimer(int Counter);
+	void cleanupStands();
 
 };
+
+
